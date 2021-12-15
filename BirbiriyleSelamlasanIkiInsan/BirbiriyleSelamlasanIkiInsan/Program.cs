b6f6f52934718e65ea17c990450178e55dab76f9
Ali Türkmen <84051961+aliturkmen4@users.noreply.github.com>
2021-12-15 22:23:59 +0300

    Add files via upload

---
+++ BirbiriyleSelamlasanIkiInsan/BirbiriyleSelamlasanIkiInsan/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirbiriyleSelamlasanIkiInsan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Insan i1 = new Insan();
+            Insan i2 = new Insan();
+            i1.Isimveyasduzenle("ali", 25);
+            i2.Isimveyasduzenle("resul", 25);
+            i1.SelamVer();
+            i2.SelamVer();
+            i1.BilgiSor();
+            i2.CevapVer();
+            i2.BilgiSor();
+            i1.CevapVer();
+            
+            //Console.WriteLine(i1.Isimveyasyazdırma());
+            //Console.WriteLine(i2.Isimveyasyazdırma());
+            Console.ReadLine();
+
+        }
+    }
+    class Insan
+    {
+        string isim;
+        int yas;
+
+        public void SelamVer()
+        {
+            Console.WriteLine("Merhaba!");
+        }
+
+        public void BilgiSor()
+        {
+            Console.WriteLine("Adın ne? ve Yaşın Kaç?");
+        }
+
+        public void CevapVer()
+        {
+            Console.WriteLine("Benim adım: "+isim+" ve yaşım: "+yas);
+        }
+         
+        public void Isimveyasduzenle(string name,int age)
+        {
+            this.isim = name;
+            this.yas = age;
+        }
+
+        public string Isimveyasyazdırma()
+        {
+            return isim + " " + yas;
+        }
+    }
+}
